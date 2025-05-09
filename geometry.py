02afc2884f3cd1656b7449e31adfbe85ff05e170
Sergej <sergej.geist@oth-regensburg.de>
2025-05-09 11:07:29 +0200

    draw move cell

--- geometry.py
+++ geometry.py
@@ -22,7 +22,11 @@ class Cell():
         self.has_right_wall = has_right_wall
         self.has_top_wall = has_top_wall
         self.has_bottom_wall = has_bottom_wall
-
+        
+        mid_x = (right_bottom_corner.x - left_top_corner.x) / 2 + left_top_corner.x
+        mid_y = (left_top_corner.y - right_bottom_corner.y) / 2 + right_bottom_corner.y
+        self._mid = Point(mid_x,mid_y)
+        
     def draw(self) -> None:
         if self.has_left_wall:
             self._win.draw_line(
@@ -36,3 +40,11 @@ class Cell():
         if self.has_bottom_wall:
             self._win.draw_line(
                 Line(self._left_bottom_corner, self._right_bottom_corner))
+            
+    def draw_move(self, to_cell, undo:bool = False): 
+        if undo:
+            color='gray'
+        else: 
+            color='red'
+        l = Line(self._mid,to_cell._mid)
+        self._win.draw_line(l,color)
--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from geometry import Cell
 
 def main():
     print("Hello from mazesolver!")
-    win = Window(400, 400)
+    win = Window(800, 800)
     line = Line(Point(0, 0), Point(200, 200))
     win.draw_line(line)
     c = Cell(Point(100, 100), Point(300, 300), win)
     c.has_bottom_wall = False
     c.draw()
+    c2 = Cell(Point(200,200),Point(400,400),win)
+    c2.draw()
+    c.draw_move(c2)
+    c3 = Cell(Point(200,500),Point(400,700),win)
+    c3.draw()
+    c2.draw_move(c3,True)
     win.wait_for_close()
 
 
